81fbbc7a0db9de3259e5c695efec818b3c2710ef
Exal0 <fenzi.alex13@gmail.com>
2024-10-29 16:08:38 +0100

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+let jouevent = document.getElementById("bouton-jouer")
+let optionevent = document.getElementById("bouton-option")
+let quittevent = document.getElementById("bouton-quitter")
+
+function lancerlejeu() {
+    jouevent.addEventListener ("click",function(){
+    console.log("debut de patie");
+    jouevent.textContent=this.appendChild
+})
+}
+lancerlejeu()
