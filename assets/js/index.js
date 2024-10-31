88804340e5b295918d36e6ab5e5152f7c753b0d6
Exal0 <fenzi.alex13@gmail.com>
2024-10-31 09:57:36 +0100

    second commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,16 @@ let jouevent = document.getElementById("bouton-jouer")
 let optionevent = document.getElementById("bouton-option")
 let quittevent = document.getElementById("bouton-quitter")
 
+let menu = document.getElementById("menu-container")
+let clownRandomize = document.getElementById("clown-rand")
+
+
 function lancerlejeu() {
     jouevent.addEventListener ("click",function(){
     console.log("debut de patie");
-    jouevent.textContent=this.appendChild
+    menu.classList.add("hidden");
+    clownRandomize.classList.remove("hidden");
+    
 })
 }
 lancerlejeu()
